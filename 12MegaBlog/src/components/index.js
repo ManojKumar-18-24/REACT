8772f9ba8b8eff1b55f6971b20b4c27770b352e9
ManojKumar-18-24 <manojkumarsamudrala836@gmail.com>
2024-12-30 17:17:54 +0530

    add real time editor and post form

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,10 +3,17 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button'
+import Input from './Input'
+import RTE from './RTE'
+
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    RTE
 }
